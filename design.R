5d4c85e85de58123d21cf422a64560d31cfbc921
Arthur Krause <amkrause@gppd-hpc.inf.ufrgs.br>
2019-05-29 16:56:16 -0300

    added tupi to DoE

--- design.R
+++ design.R
@@ -38,3 +38,23 @@
                 type="csv",
                 replace=TRUE,
                 response.names=c("time"));
+
+  pi_doe <- fac.design (
+           nfactors=2,
+           replications=30,
+           repeat.only=FALSE,
+           blocks=1,
+           randomize=TRUE,
+           seed=10373,
+           nlevels=c(5, 5),
+           factor.names=list(
+                version=c("base", "critical", "false_sharing", "reduction", "seq"),
+                threads=c(1, 2, 4, 8, 16)
+           ));
+
+  export.design(pi_doe,
+                path=".",
+                filename="pi_doe_tupi",
+                type="csv",
+                replace=TRUE,
+                response.names=c("time"));
