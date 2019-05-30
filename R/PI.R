bf69e319b58a59f7e70eee477c7a3bafb2b16215
Arthur Krause <amkrause@gppd-hpc.inf.ufrgs.br>
2019-05-30 09:58:02 -0300

    removi arquivos do anderson

--- R/PI.R
+++
@@ -1,9 +0,0 @@
-df <- read_csv("") %>%
-select(-metric, -machine) -> df
-
-df %>%
-group_by(app, threads) %>%
-summarise(mean = mean(value),
-sd = sd(value),
-N = n()) %>%
-arrange(app, threads) -> df
